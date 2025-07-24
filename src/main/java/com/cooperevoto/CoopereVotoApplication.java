cce97ac67c3201c6cae3ee9dae24db6b91bd208e
saulocapistrano <saulocapistrano@gmail.com>
2025-07-24 18:55:22 -0300

    chore: inicializa projeto Spring Boot com dependências básicas (JPA, Swagger, Liquibase, MapStruct, Testes)

---
+++ src/main/java/com/cooperevoto/CoopereVotoApplication.java
@@ -0,0 +1,13 @@
+package com.cooperevoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopereVotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoopereVotoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cooperevoto/CoopereVotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooperevoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopereVotoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
