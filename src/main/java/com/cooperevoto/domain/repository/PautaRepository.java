c43f414a1d410cf8132aa04a9b3a1d53c6c379ae
saulocapistrano <saulocapistrano@gmail.com>
2025-07-24 20:56:04 -0300

    feat(repository): define interface JPA para persistência de pautas

---
+++ src/main/java/com/cooperevoto/domain/repository/PautaRepository.java
@@ -0,0 +1,7 @@
+package com.cooperevoto.domain.repository;
+
+import com.cooperevoto.domain.model.Pauta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PautaRepository extends JpaRepository<Pauta, Long> {
+}
