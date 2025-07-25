dda377da8fa316e92fb56d2419cdd291d65c84a5
saulocapistrano <saulocapistrano@gmail.com>
2025-07-24 22:15:35 -0300

    refactor(dto): adicionar construtor completo para facilitar uso em testes unitários

--- src/main/java/com/cooperevoto/api/v1/dto/request/PautaRequest.java
+++ src/main/java/com/cooperevoto/api/v1/dto/request/PautaRequest.java
@@ -1,9 +1,11 @@
 package com.cooperevoto.api.v1.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class PautaRequest {
 
     @NotBlank(message = "Título é obrigatório")
