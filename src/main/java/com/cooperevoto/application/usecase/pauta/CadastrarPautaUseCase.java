8ac748332542a8485f225980b6815a60c9b257ee
saulocapistrano <saulocapistrano@gmail.com>
2025-07-24 21:42:31 -0300

    refactor(usecase): mover CadastrarPautaUseCase para subpacote pauta e ajustar referência

--- src/main/java/com/cooperevoto/application/usecase/pauta/CadastrarPautaUseCase.java
+++ src/main/java/com/cooperevoto/application/usecase/pauta/CadastrarPautaUseCase.java
@@ -1,4 +1,4 @@
-package com.cooperevoto.application.usecase;
+package com.cooperevoto.application.usecase.pauta;
 
 import com.cooperevoto.api.v1.dto.request.PautaRequest;
 import com.cooperevoto.api.v1.dto.response.PautaResponse;
