70a227e571922681d60c6689ed3fe500559ad174
saulocapistrano <saulocapistrano@gmail.com>
2025-07-24 22:15:36 -0300

    refactor(dto): ajustar construtor de PautaResponse para uso em mocks de testes

--- src/main/java/com/cooperevoto/api/v1/dto/response/PautaResponse.java
+++ src/main/java/com/cooperevoto/api/v1/dto/response/PautaResponse.java
@@ -1,10 +1,14 @@
 package com.cooperevoto.api.v1.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class PautaResponse {
     private Long id;
     private String titulo;
     private String descricao;
+
+
 }
