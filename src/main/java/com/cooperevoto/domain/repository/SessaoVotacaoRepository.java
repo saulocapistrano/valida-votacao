ff5aaa7d3984148cf584bcf8b7108723ad5d5079
saulocapistrano <saulocapistrano@gmail.com>
2025-07-25 17:03:21 -0300

    feat(repository): adicionar método de busca por pautaId em SessaoVotacaoRepository

--- src/main/java/com/cooperevoto/domain/repository/SessaoVotacaoRepository.java
+++ src/main/java/com/cooperevoto/domain/repository/SessaoVotacaoRepository.java
@@ -3,6 +3,10 @@ package com.cooperevoto.domain.repository;
 import com.cooperevoto.domain.model.SessaoVotacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SessaoVotacaoRepository extends JpaRepository<SessaoVotacao, Long> {
     boolean existsByPautaId(Long pautaId);
+    Optional<SessaoVotacao> findByPautaId(Long pautaId);
+
 }
