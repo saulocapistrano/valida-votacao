32e127014e80eaad4be06b9b64f6447c67c01eb1
saulocapistrano <saulocapistrano@gmail.com>
2025-07-28 12:01:31 -0300

    test(test): adiciona a anotação para uso do prifile test

--- src/test/java/com/cooperevoto/CoopereVotoApplicationTests.java
+++ src/test/java/com/cooperevoto/CoopereVotoApplicationTests.java
@@ -2,8 +2,10 @@ package com.cooperevoto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CoopereVotoApplicationTests {
 
     @Test
